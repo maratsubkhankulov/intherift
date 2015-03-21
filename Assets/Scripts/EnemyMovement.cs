b29a146291efd0a58437f5e0cfc7afa52943a771
Andrius Dagys <andriusdag@gmail.com>
2015-03-21 16:16:06 +0000

    Added player & enemy scripts

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+	Transform player;               // Reference to the player's position.
+	NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+	void Start ()
+	{
+		// Set up the references.
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <NavMeshAgent> ();
+
+	}
+	
+	
+	void Update ()
+	{
+
+		nav.SetDestination (player.position);
+
+	} 
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			Debug.Log("Attacking");
+			Attack ();
+		}
+	}
+
+	void Attack()
+	{
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+
+		foreach (var enemy in enemies) 
+		{
+			if (IsVisible(enemy))
+			{
+				Debug.Log ("Enemy seen");
+			} else {
+				Debug.Log ("not seen enemy");
+			}
+		}
+	}
+
+	bool IsVisible(GameObject o) {
+		return o.renderer.isVisible;
+	}
+
+	void OnTriggerStay(Collider other) {
+
+		var rotation = transform.rotation;
+
+	}
+}
