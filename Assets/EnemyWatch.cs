6ae67c47a46ac5d3853f711c9f82948579ce2da5
Marat Subkhankulov <msubkhankulov@tp-marats.nimblestorage.com>
2015-03-21 16:04:13 +0000

    added OVR and enemy seeing

---
+++ Assets/EnemyWatch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWatch : MonoBehaviour {
+
+	private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		foreach (GameObject enemy in enemies) {
+			if (enemy != null)
+			if (IsVisibleFrom (enemy.renderer, mainCamera)) {
+				
+				Debug.Log ("seen enemy");
+			}
+		}
+	}
+
+	public bool IsVisibleFrom(Renderer renderer, Camera camera)
+	{
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+}
